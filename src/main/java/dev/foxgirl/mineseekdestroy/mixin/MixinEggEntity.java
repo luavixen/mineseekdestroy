45591577f7e6847d814617981edf171bcadf28ef
Lua MacDougall <lua@foxgirl.dev>
2022-11-09 12:24:57 -0800

    Sync changes

--- src/main/java/dev/foxgirl/mineseekdestroy/mixin/MixinEggEntity.java
+++ src/main/java/dev/foxgirl/mineseekdestroy/mixin/MixinEggEntity.java
@@ -2,7 +2,7 @@ package dev.foxgirl.mineseekdestroy.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.thrown.EggEntity;
-import net.minecraft.world.ModifiableWorld;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -12,9 +12,9 @@ public abstract class MixinEggEntity {
 
     @Redirect(
         method = "onCollision(Lnet/minecraft/util/hit/HitResult;)V",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ModifiableWorld;spawnEntity(Lnet/minecraft/entity/Entity;)Z")
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z")
     )
-    private boolean mineseekdestroy$hookOnCollision(ModifiableWorld world, Entity entity) {
+    private boolean mineseekdestroy$hookOnCollision(World world, Entity entity) {
         return false;
     }
 
