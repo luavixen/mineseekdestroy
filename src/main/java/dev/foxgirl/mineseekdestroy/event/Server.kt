0dd61648cf00dc5e6a6cf40b45937d706a503864
Lua MacDougall <lua@foxgirl.dev>
2023-06-16 18:59:00 -0700

    Sync changes

--- src/main/java/dev/foxgirl/mineseekdestroy/event/Server.kt
+++ src/main/java/dev/foxgirl/mineseekdestroy/event/Server.kt
@@ -1,44 +1,36 @@
 package dev.foxgirl.mineseekdestroy.event
 
-import io.javalin.Javalin
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import org.apache.logging.log4j.Level
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.core.Filter
-import org.apache.logging.log4j.core.LogEvent
-import org.apache.logging.log4j.core.Logger
-import org.apache.logging.log4j.core.filter.AbstractFilter
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.websocket.api.WebSocketAdapter
+import org.eclipse.jetty.websocket.server.JettyServerUpgradeRequest
+import org.eclipse.jetty.websocket.server.JettyServerUpgradeResponse
+import org.eclipse.jetty.websocket.server.JettyWebSocketServerContainer
+import org.eclipse.jetty.websocket.server.config.JettyWebSocketServletContainerInitializer
 
 fun start() {
 
-    (LogManager.getRootLogger() as Logger).addFilter(object : AbstractFilter() {
-        override fun filter(event: LogEvent): Filter.Result {
-            if (event.level.isMoreSpecificThan(Level.INFO)) {
-                Bus.publish(MessageEvent(event.message.formattedMessage))
+    val server = Server()
+    val connector = ServerConnector(server).also { server.addConnector(it) }
+
+    connector.port = 8080
+
+    val context = ServletContextHandler(ServletContextHandler.SESSIONS)
+
+    context.contextPath = "/"
+    server.handler = context
+
+    JettyWebSocketServletContainerInitializer.configure(context) { _, wsContainer: JettyWebSocketServerContainer ->
+        // Configure default max size
+        wsContainer.maxTextMessageSize = 65535
+
+        // Add websockets
+        wsContainer.addMapping("/listen") { jettyServerUpgradeRequest: JettyServerUpgradeRequest, jettyServerUpgradeResponse: JettyServerUpgradeResponse ->
+            object : WebSocketAdapter() {
+
             }
-            return Filter.Result.NEUTRAL
-        }
-    })
-
-    val app = Javalin.create()
-
-    app.ws("/") { ws ->
-        var subscription: Bus.Subscription? = null
-        ws.onConnect { ctx ->
-            subscription = Bus.subscribe { event -> ctx.send(Json.encodeToString(event)) }
-        }
-        ws.onError { ctx ->
-            ctx.closeSession()
-            subscription?.unsubscribe()
-        }
-        ws.onClose { ctx ->
-            subscription?.unsubscribe()
         }
     }
-
-    app.start("127.0.0.1", 25566)
-
-    Runtime.getRuntime().addShutdownHook(Thread { app.stop() })
 
 }
